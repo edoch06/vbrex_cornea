7dacc71f94e837e794332fd244922d2fb4ce5284
orca <edoch06@gmail.com>
2024-11-17 15:23:32 +0300

    add blood group

---
+++ Cornea/AddBlGroup.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.Common
+Imports System.Data.Odbc
+Imports System.Data.SqlClient
+Imports System.Drawing.Printing
+Imports System.IO
+Imports System.Text
+Imports System.Media
+Imports System.Windows.Forms
+
+Public Class AddBlGroup
+
+
+
+    Private Const WM_SYSCOMMAND As Integer = &H112
+    Private Const SC_MAXIMIZE As Integer = &HF030
+    Private Const WM_NCLBUTTONDBLCLK As Integer = &HA3
+    Private Const HTCAPTION As Integer = &H2
+    Private widthint As Integer = 498
+    Private heightint As Integer = 286
+    Private init_widthint As Integer = 0
+    Private init_heightint As Integer = 0
+    '498, 286
+
+   
+End Class
